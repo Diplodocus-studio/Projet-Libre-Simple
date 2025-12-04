f7136cdd396732eb7632d4035e3acdee893e6db9
Diplodocus-studio <mateo.hiolle@gmail.com>
2025-12-04 19:28:22 +0100

    Start changing them

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-  ],
+  content: [],
   theme: {
-    extend: {},
+    extend: {
+       colors: {
+        'platinum': '#EFEFEF',
+        'jet-black': '#2B303A',
+        'frosted-blue': '#92DCE5',
+        'lavender-blush': '#EEE5E9',
+        'custom-grey': '#7C7C7C',
+        'burnt-tangerine': '#D64933',
+      },
+    },
   },
   plugins: [],
   "editor.quickSuggestions": {
