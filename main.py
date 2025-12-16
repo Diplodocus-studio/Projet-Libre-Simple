a9b6ef28b15fb5b605004654c9b11d53202e6c99
Diplodocus-studio <mateo.hiolle@gmail.com>
2025-12-16 13:46:13 +0100

    Add embeded code and media classification

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, session, redirect, url_for
 import pymongo
+from bson import ObjectId
+import re
 app = Flask(__name__)
 
 client = pymongo.MongoClient("mongodb+srv://mateohiolle_db_user:ZLy9PpJT7ixl5hOM@post.qt7jab2.mongodb.net/?appName=Post")
@@ -94,19 +96,42 @@ def publish():
         db_posts = db['posts']
         title = request.form['title']
         media = request.form['media']
+
+        if bool(re.match(
+                r'^(https?:\/\/)?(www\.)?(youtube\.com\/(watch\?v=|shorts\/|embed\/)|youtu\.be\/)[^?\s&]+.*$',
+                media, re.IGNORECASE)):
+            
+            media_type = "yt"
+            media = (m := re.search(r'(?:youtube\.com/(?:watch\?v=|embed/|shorts/)|youtu\.be/)([A-Za-z0-9_-]{11})', media)) and m.group(1)
+
+        elif bool(re.match(r'^(https?:\/\/)?(www\.)?itch\.io\/embed-upload\/\d+(\?.*)?$', media, re.IGNORECASE)):
+            media_type = "itch"
+            media = (m := re.search(r'itch\.io/(?:embed-upload|embed)/(\d+)', media) and m.group(1))
+
+        elif bool(re.match(r'^(https?:\/\/)?(www\.)?scratch\.mit\.edu\/projects\/\d+\/?(\?.*)?$', media, re.IGNORECASE)):
+            media_type = "scratch"
+            media = ((m := re.search(r'scratch\.mit\.edu/projects/(\d+)', media)) and m.group(1))
+
+        elif bool(re.match(r'^(https?:\/\/)?[^\s"\']+\.(png|jpe?g|gif|webp|bmp|svg|ico|avif)(\?.*)?$', media, re.IGNORECASE)):
+            media_type = "img"
+        else:
+            return render_template('publish.html', error="Please enter a supported media type")
+        
+
         content = request.form['content']
         author = session['user']
 
         if title and content and media:
             db_posts.insert_one({
                 'title' : title,
+                'media_type' : media_type,
                 'media' : media,
                 'content' : content,
                 'author' : author
             })
             return redirect(url_for('index'))
         else:
-            return render_template('publish.html', erreur="Please Fill All Fields")
+            return render_template('publish.html', error="Please Fill All Fields")
     else:
         return render_template('publish.html')
 
@@ -114,8 +139,8 @@ def publish():
 @app.route('/<id>')
 def open_post(id):
     db_posts = db['posts']
-    post = db_posts.find_one({'_id' : "id"})
-    print("sucsess")
+    post = db_posts.find_one({'_id' : ObjectId(id)})
+
     return render_template('post.html', post=post)
 
 
